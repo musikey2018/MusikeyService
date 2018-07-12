f24d513016fdc54638a60b0174f371c10532e09e
bilawal.aziz <bilawal.aziz@gmail.com>
2018-07-12 16:27:27 -0700

    -- added file upload for audio

--- controllers/ChatController.js
+++ controllers/ChatController.js
@@ -13,7 +13,7 @@ function ChatController() {
     var chatService = require('../core/Chat')
 
 
-    that.registerUserForChat = function (req, res, next) {
+    that.registerUserForChat = function (req, res) {
 
         var useremail = req.params.email;
         var userId =  req.params.userId;
@@ -34,6 +34,30 @@ function ChatController() {
         });
 
     }
+
+    that.CreatePrivateChat = function (req, res) {
+
+      var user1email = req.params.email;
+      var user1Id =  req.params.userId;
+      var user2email = req.params.otheremail;
+      var user2Id =  req.params.otheruserId;
+
+      console.log("ChatController.registerUserForChat() email request ", useremail);
+      console.log("ChatController.registerUserForChat() eventId ", userId);
+      
+      chatService.createUser({
+          id: userId,
+          name: useremail,
+        })
+          .then(() => {
+            console.log('User registered for chat successfully');
+            return res.send(generalResponse.sendSuccessResponse("registered for chat successfully", 200, {'userId':userId}));
+          }).catch((err) => {
+            console.log(err);
+            return res.send(generalResponse.sendFailureResponse("Error Occured :something went wrong while registering for chat", 400, null));
+      });
+
+  }
 
     return that;
 
